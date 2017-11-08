7bdea395c4c4a1add35df3a7c9ae290a37641f83
Gonzalo Adrián Vallerino <gonzalo.vallerino.so@softtekosde.com>
2017-11-08 18:16:51 -0300

    Se inicializa el proyecto x-men

---
+++ src/main/java/ar/com/XmenML/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ar.com.XmenML;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XMenMlApplication.class);
+	}
+
+}
---
+++ src/main/java/ar/com/XmenML/XMenMlApplication.java
@@ -0,0 +1,12 @@
+package ar.com.XmenML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XMenMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XMenMlApplication.class, args);
+	}
+}
