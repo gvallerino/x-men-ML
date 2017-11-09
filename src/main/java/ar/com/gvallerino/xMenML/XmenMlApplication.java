1b776f6a294dbe9d68324767a4552915c3a48577
Gonzalo Adrián Vallerino <gonzalo.vallerino.so@softtekosde.com>
2017-11-09 12:06:38 -0300

    Se crea un rest controller de prueba

--- src/main/java/ar/com/gvallerino/xMenML/XmenMlApplication.java
+++ src/main/java/ar/com/gvallerino/xMenML/XmenMlApplication.java
@@ -2,10 +2,10 @@ package ar.com.gvallerino.xMenML;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class XmenMlApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmenMlApplication.class, args);
