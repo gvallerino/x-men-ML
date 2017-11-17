57218d2976ce061484d47831603f0548fa80261c
gvallerino <gvallerino@gmail.com>
2017-11-17 01:18:18 -0300

    Se modifica un log

--- src/main/java/ar/com/gvallerino/xMenML/service/impl/DnaCacheManagerServiceImpl.java
+++ src/main/java/ar/com/gvallerino/xMenML/service/impl/DnaCacheManagerServiceImpl.java
@@ -54,13 +54,12 @@ public class DnaCacheManagerServiceImpl implements DnaCacheManagerService {
 			Dna dna = mapDna.getValue();
 			
 			try {
-				LOGGER.error("Guardando en cache el ADN: " + key);
+				LOGGER.error("Guardando en base de datos el ADN: " + key);
 				dnaDAO.save(dna);
 				myCache.remove(key);
 			} catch (Exception e) {
 				LOGGER.error("No se pudo almacenar en la base de datos el ADN " + dna.getId());
 			}
-			LOGGER.error("Limpiando cache");
 			idDna = 0L;
 		}
 	}
